bf763c663d5c780adc45a0fe123d32ecf378b5ca
theresa le <trle@wolfmail.stritch.edu>
2016-06-29 11:03:33 -0500

    Field Variables
    fixed math.random, made field input variables: confirmed with alert,
    button onclick=onSubmit();

--- js/lemonadeStand.js
+++ js/lemonadeStand.js
@@ -7,18 +7,24 @@
 
 function LemonadeStand(){
 	this.peopleArray = [];
-	this.avgBuy = Math.random();
 	this.addPeople = function(num){
 		var people = num;
 		for(var i=0; i<people; i++){
-			var newPerson = i+1 + ":" + this.avgBuy;
+			var newPerson = i+1 + ":" + Math.random();
 			this.peopleArray.push(newPerson);
 		}
 	console.log(this.peopleArray);
 	}
+
+	this.onSubmit = function(){
+		var daysSimulate = document.getElementById("daysSimulate").value;  
+		var avgPeople = document.getElementById("avgPeople").value;  
+		var cost = document.getElementById("cost").value;  
+		var price = document.getElementById("price").value;  
+		alert(daysSimulate + avgPeople + cost + price);
+	}
+
 }
 
-
-
-var Test = new LemonadeStand();
-Test.addPeople(5);
+var myTest = new LemonadeStand();
+myTest.addPeople(5);
